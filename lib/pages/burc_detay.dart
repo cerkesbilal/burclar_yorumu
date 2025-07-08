8002988bd7f4fa2c20718c05a0c5c420b1df5328
“cerkesbilal” <bilalkarademir86@gmail.com>
2025-07-08 16:00:11 +0300

    son detaylar

--- lib/pages/burc_detay.dart
+++ lib/pages/burc_detay.dart
@@ -1,12 +1,54 @@
 import 'package:burclar_yorumu/model/burc.dart';
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   final Burc secilenBurc;
   const BurcDetay({super.key, required this.secilenBurc});
 
   @override
+  State<BurcDetay> createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Color appBarRengi = Colors.pink;
+
+  late PaletteGenerator _generator;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 250,
+            pinned: false,
+            //snap: true,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text("${widget.secilenBurc.burcAdi} Burcu ve Özellikleri"),
+              background: Image.asset(
+                "resimler/${widget.secilenBurc.burcBuyukResim}",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          //dikkat eğer Custom içinde normalde widget kullanmak istiyorsanız
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Text(
+                  widget.secilenBurc.burcDetay,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
