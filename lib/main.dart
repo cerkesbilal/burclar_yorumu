b5d6f242a4430a4275cfdf4d898f09f4fdb3c98e
“cerkesbilal” <bilalkarademir86@gmail.com>
2025-07-08 15:10:19 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:burclar_yorumu/pages/burc_listesi.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Colors.red),
+        primarySwatch: Colors.pink,
+        useMaterial3: false,
+      ),
+      home: BurcListesi(),
+    );
+  }
+}
